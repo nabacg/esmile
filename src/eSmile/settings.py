af87a5e480ac79cb8ee9a11e0ca6586d1a36879a
gmc <nabacg@gmail.com>
2011-01-08 17:53:44 +0000

    usuniete problematyczne eSmile.urls

--- src/eSmile/settings.py
+++ src/eSmile/settings.py
@@ -94,7 +94,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'eSmile.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
