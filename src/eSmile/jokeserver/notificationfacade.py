7cd75fc0418f2ce2d597f323454a35a3e830bed8
gmc <nabacg@gmail.com>
2011-02-09 19:43:05 +0000

    Tlumacznie na polski

--- src/eSmile/jokeserver/notificationfacade.py
+++ src/eSmile/jokeserver/notificationfacade.py
@@ -13,7 +13,7 @@ def send_joke(joke, receiver_list):
  
 # tutaj trzeba zaprzadz django-template'y w towrzenie ladnego maila
 def get_email_content(joke):
-     new_email = Template("{{ user_name }}'s daily joke:\n\n {{ joke_text }} \n\n\n \t\t\t posted on {{ posted_on }} \n\n\n------------------------------------------------------------------------------------------------------\n\n \t\t\t This joke was delivered to you using {{esmile_url}}")
+     new_email = Template("Poranny żart od {{ user_name }}:\n\n {{ joke_text }} \n\n\n \t\t\t dodany {{ posted_on }} \n\n\n------------------------------------------------------------------------------------------------------\n\n \t\t\t Żart wysłano za pośrednictwem {{esmile_url}}")
      context = Context({
                         "user_name": joke.owner.username,
                         "joke_text": joke.value.replace('<br/>', '\n'),
