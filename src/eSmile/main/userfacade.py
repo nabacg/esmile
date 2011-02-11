61397f02084c562450f5eff8dfcb75387479d9a2
gmc <nabacg@gmail.com>
2011-02-11 20:20:47 +0000

    Zmiana sortowania aktywnych userow na stronie glownej na ilosc uzytkownikow

--- src/eSmile/main/userfacade.py
+++ src/eSmile/main/userfacade.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
+from django.db.models import Count
 
 def get_most_active_users():
-    most_active = User.objects.order_by('-last_login')[0:10]
-    
+   # most_active = User.objects.order_by('-last_login')[0:10]
+    most_active = User.objects.annotate(num_of_jokes = Count('jokes')).order_by('-num_of_jokes')[0:10]
     return most_active
