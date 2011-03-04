e144be118dad881e27cb347db5d78d7957c57664
gmc <nabacg@gmail.com>
2011-03-04 20:15:34 +0000

    hasla do mejli

--- src/eSmile/settings.py
+++ src/eSmile/settings.py
@@ -32,7 +32,7 @@ DATABASES = {
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'nabacg@gmail.com'
-EMAIL_HOST_PASSWORD = 'szcz*8ek'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = "nabacg@gmail.com"
 
